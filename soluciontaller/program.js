3f18e018a972935d8e8ff582df95ef07503ce93d
Mike RR <michaelandresrr@gmail.com>
2025-03-02 17:11:49 -0500

    Update map view coordinates for improved accuracy in program.js

--- soluciontaller/program.js
+++ soluciontaller/program.js
@@ -2,7 +2,7 @@
 window.jsPDF = window.jspdf.jsPDF;
 
 // Initialize the Leaflet map and set the view to La Coruña-Bogotá
-const map = L.map('map').setView([4.578330940484695, -74.1527], 17); // Coordinates for La Coruña-Bogotá
+const map = L.map('map').setView([4.5786, -74.15265], 17); // Coordinates for La Coruña-Bogotá
 
 // Add OpenStreetMap tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
