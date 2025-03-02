a4d0025d946089710c8d22930b11de1c6da10557
Mike RR <michaelandresrr@gmail.com>
2025-03-02 17:09:19 -0500

    Adjust map view coordinates and increase maximum zoom level in program.js

--- soluciontaller/program.js
+++ soluciontaller/program.js
@@ -2,11 +2,11 @@
 window.jsPDF = window.jspdf.jsPDF;
 
 // Initialize the Leaflet map and set the view to La Coruña-Bogotá
-const map = L.map('map').setView([4.578330940484695, -74.15273450917056], 16); // Coordinates for La Coruña-Bogotá
+const map = L.map('map').setView([4.578330940484695, -74.1527], 17); // Coordinates for La Coruña-Bogotá
 
 // Add OpenStreetMap tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 20,
 }).addTo(map);
 
 let loadPolygon = async function() {
